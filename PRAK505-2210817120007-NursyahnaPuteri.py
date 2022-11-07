68be7c59b7a3f5565781b6d230e02da47dc5e951
Nursyahna Puteri <114853207+NurSyahnaPuterii@users.noreply.github.com>
2022-11-08 01:56:46 +0800

    first commit

---
+++ PRAK505-2210817120007-NursyahnaPuteri.py
@@ -0,0 +1,12 @@
+def Biodata (tahun, nama, asal ) :
+    tahun_sekarang =2020
+    tahun = int (tahun)
+    print ("Perkenalkan Nama Saya : ", nama)
+    print ("Umur Saya : ",tahun_sekarang - tahun)
+    print ("Saya Adalah Angkatan : ", tahun_sekarang)
+    print ("Asal Saya dari : ", asal)
+
+tahunlahir = int (input ())
+Namaku = input ()
+Asal = input ()
+Biodata (tahunlahir, Namaku, Asal)
